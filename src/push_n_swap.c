59889a6eb84f3cd35d8a8be9e5ca971eb9aaa286
Sarah BROCHARD <sbrochar@e2r9p11.42.fr>
2017-11-07 15:34:54 +0100

    sorts 500 elements in ~5500 coups

--- src/push_n_swap.c
+++ src/push_n_swap.c
@@ -6,7 +6,7 @@
 /*   By: sbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/28 21:10:15 by sbrochar          #+#    #+#             */
-/*   Updated: 2017/11/06 17:22:14 by sbrochar         ###   ########.fr       */
+/*   Updated: 2017/11/07 13:42:17 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,18 @@ static void			insert_sorted_node(t_dblist **n_smallest, int i, size_t n)
 	}
 }
 
-static int			find_n_smallest(t_clist **a, size_t n)
+static int			find_n_smallest(t_clist **list, size_t n)
 {
 	t_dblist		*n_smallest;
 	t_node			*cur;
 	int				ret;
 
 	n_smallest = create_dblist();
-	cur = (*a)->start;
+	cur = (*list)->start;
 	ret = 0;
 	if (n_smallest)
 	{
-		while (cur != (*a)->end)
+		while (cur != (*list)->end)
 		{
 			insert_sorted_node(&n_smallest, *((int *)(cur->content)), n);
 			cur = cur->next;
@@ -54,12 +54,12 @@ static int			find_n_smallest(t_clist **a, size_t n)
 		insert_sorted_node(&n_smallest, *((int *)(cur->content)), n);
 	}
 
-/*	cur = n_smallest->start;
-	while (cur)
-	{
+	/*	cur = n_smallest->start;
+		while (cur)
+		{
 		ft_printf("%d\n", *((int *)(cur->content)));
 		cur = cur->next;
-	}*/
+		}*/
 
 	ret = *((int *)(n_smallest->end->content));
 	free_dblist(&n_smallest);
@@ -89,24 +89,46 @@ static t_bool		choose_rotating_way(t_clist **a, int n_smallest)
 	return (check_ra < check_rra);
 }
 
-static void			push_n_smallest(t_clist **a, t_clist **b, size_t n, int	n_smallest)
+static void			push_n_smallest(t_clist **a, t_clist **b, size_t n, int	n_smallest, t_bool isa, t_bool first)
 {
 	size_t			i;
 	t_bool			ra;
 
 	i = 0;
-	while (i < n)
+	if (isa)
 	{
-		ra = choose_rotating_way(a, n_smallest);
-		while (*((int *)((*a)->start->content)) > n_smallest)
+		while (i < n)
 		{
-			if (ra)
-				rotate_a(a, b, B_TRUE);
+			if (first)
+				ra = 1;
 			else
-				reverse_rotate_a(a, b, B_TRUE);
+				ra = choose_rotating_way(a, n_smallest);
+			while (*((int *)((*a)->start->content)) > n_smallest)
+			{
+				if (ra)
+					rotate_a(a, b, B_TRUE);
+				else
+					reverse_rotate_a(a, b, B_TRUE);
+			}
+			push_b(a, b, B_TRUE);
+			i++;
 		}
-		push_b(a, b, B_TRUE);
-		i++;
+	}
+	else
+	{
+		while (i < n)
+		{
+			ra = choose_rotating_way(b, n_smallest);
+			while (*((int *)((*b)->start->content)) > n_smallest)
+			{
+				if (ra)
+					rotate_b(a, b, B_TRUE);
+				else
+					reverse_rotate_b(a, b, B_TRUE);
+			}
+			push_a(a, b, B_TRUE);
+			i++;
+		}
 	}
 }
 
@@ -115,17 +137,55 @@ void				push_n_swap(t_clist **a, t_clist **b, size_t nb_elem)
 	size_t			n;
 	size_t			rest;
 	int				n_smallest;
-	
+
 	//check cas particuliers
-	nb_elem = (size_t)nb_elem;
-	n = 10 * nb_elem / 100;
-	while ((*a)->nb_nodes > n)
+	//n = 10 * nb_elem / 100;
+
+	n = 0;
+	if (nb_elem == 3)
+		ft_printf("3 elem");
+	else if (nb_elem == 5)
+		ft_printf("5 elem");
+	else
 	{
+		if (nb_elem > 200)
+		{
+			n = nb_elem / 4;
+			while (n > (10 * nb_elem / 100))
+			{
+				while ((*a)->nb_nodes > n)
+				{
+					n_smallest = find_n_smallest(a, n);
+					push_n_smallest(a, b, n, n_smallest, B_TRUE, B_TRUE);
+				}
+				rest = (*a)->nb_nodes;
+				n_smallest = find_n_smallest(a, n);
+				push_n_smallest(a, b, rest, n_smallest, B_TRUE, B_TRUE);
+				n /= 4;
+				while ((*b)->nb_nodes > n)
+				{
+					n_smallest = find_n_smallest(b, n);
+					push_n_smallest(a, b, n, n_smallest, B_FALSE, B_FALSE);
+				}
+				rest = (*b)->nb_nodes;
+				n_smallest = find_n_smallest(b, n);
+				push_n_smallest(a, b, rest, n_smallest, B_FALSE, B_FALSE);
+				n /= 4;
+			}
+		}
+		if (!n)
+			n = 18 * nb_elem / 100;
+		while ((*a)->nb_nodes > n)
+		{
+			n_smallest = find_n_smallest(a, n);
+			if (nb_elem > 200)
+				push_n_smallest(a, b, n, n_smallest, B_TRUE, B_FALSE);
+			else
+				push_n_smallest(a, b, n, n_smallest, B_TRUE, B_TRUE);
+		}
+		rest = (*a)->nb_nodes;
 		n_smallest = find_n_smallest(a, n);
-		push_n_smallest(a, b, n, n_smallest);
+		push_n_smallest(a, b, rest, n_smallest, B_TRUE, B_FALSE);
+		insert_in_a(a, b, rest, n);
 	}
-	rest = (*a)->nb_nodes;
-	n_smallest = find_n_smallest(a, n);
-	push_n_smallest(a, b, rest, n_smallest);
-	insert_in_a(a, b, rest, n);	
 }
